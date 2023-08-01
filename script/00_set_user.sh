0859b0b75e7cd282a8883f77b05b005e76d6498c
25th-Night <brown_eyed87@naver.com>
2023-08-01 22:25:02 +0900

    00_set_user.sh: 유저 생성 스크립트 추가

---
+++ script/00_set_user.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+USERNAME=""
+PASSWORD=""
+MANUAL="Usage: $0 [-u username -p password]"
+
+while getopts "u:p:" option
+do
+	case $option in
+		u)
+			USERNAME=$OPTARG
+			;;
+		p)
+			PASSWORD=$OPTARG
+			;;
+		*)
+			echo $MANUAL
+			exit 1
+			;;
+	esac
+done
+
+if [ -z "$USERNAME" ] || [ -z "$PASSWORD" ]
+then
+	echo "username and password are required"
+	echo $MANUAL
+	exit 1
+fi
+
+echo "add user"
+useradd -s /bin/bash -d /home/lion -m $USERNAME
+
+echo "set password"
+echo "$USERNAME:$PASSWORD" | chpasswd
+
+echo "set sudo"
+usermod -aG sudo $USERNAME
+echo "$USERNAME ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers.d/$USERNAME
+
+echo "done"
