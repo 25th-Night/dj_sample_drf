e166b321ae459573a289873686f266e4b7caa4c1
25th-Night <brown_eyed87@naver.com>
2023-08-07 09:22:11 +0900

    05_ncr_push.sh: 이미지 태그가 존재하면 삭제하고 다시 생성하는 로직 추가

--- script/05_ncr_push.sh
+++ script/05_ncr_push.sh
@@ -2,28 +2,32 @@
 
 source "./.env"
 
-# Create Image Tags for Image Push to NCR"
+# Create Django Image Tag in Local
 if [ -n $(docker images | grep -q "$LOCAL_DJANGO_IMAGE_NAME" | grep -q "$LOCAL_DJANGO_IMAGE_TAG") ]
 then
-	if [ -n $(docker images | grep -q "$NCR_ADDRESS/$NCR_DJANGO_IMAGE_NAME" | grep -q "$NCR_DJANGO_IMAGE_TAG") ]
-	then 
-		echo "Remove eaxisting Django Image Tag for NCR"
-		docker tag $LOCAL_DJANGO_IMAGE_NAME:$LOCAL_DJANGO_IMAGE_TAG $NCR_ADDRESS/$NCR_DJANGO_IMAGE_NAME:$NCR_DJANGO_IMAGE_TAG
-	fi
-	echo "Create Django Image Tag for NCR"
-	docker tag $LOCAL_DJANGO_IMAGE_NAME:$LOCAL_DJANGO_IMAGE_TAG $NCR_ADDRESS/$NCR_DJANGO_IMAGE_NAME:$NCR_DJANGO_IMAGE_TAG
+    echo "Remove eaxisting Django Image in Local"
+    docker rmi -f $LOCAL_DJANGO_IMAGE_NAME:$LOCAL_DJANGO_IMAGE_TAG
 fi
+echo "Create Django Image Tag in Local"
+docker build -t $LOCAL_DJANGO_IMAGE_NAME:$LOCAL_DJANGO_IMAGE_TAG -f Dockerfile_django .
 
+# Create Django Image Tag for NCR
+echo "Create Django Image Tag for NCR"
+docker tag $LOCAL_DJANGO_IMAGE_NAME:$LOCAL_DJANGO_IMAGE_TAG $NCR_ADDRESS/$NCR_DJANGO_IMAGE_NAME:$NCR_DJANGO_IMAGE_TAG
+
+
+# Create NGINX Image Tag in Local
 if [ -n $(docker images | grep -q "$LOCAL_NGINX_IMAGE_NAME" | grep -q "$LOCAL_NGINX_IMAGE_TAG") ]
 then
-	if [ -n $(docker images | grep -q "$NCR_ADDRESS/$NCR_NGINX_IMAGE_NAME" | grep -q "$NCR_NGINX_IMAGE_TAG") ]
-	then 
-		echo "Remove eaxisting Nginx Image Tag for NCR"
-		docker tag $LOCAL_NGINX_IMAGE_NAME:$LOCAL_NGINX_IMAGE_TAG $NCR_ADDRESS/$NCR_NGINX_IMAGE_NAME:$NCR_NGINX_IMAGE_TAG
-	fi
-	echo "Create NGINX Image Tag for NCR"
-	docker tag $LOCAL_NGINX_IMAGE_NAME:$LOCAL_NGINX_IMAGE_TAG $NCR_ADDRESS/$NCR_NGINX_IMAGE_NAME:$NCR_NGINX_IMAGE_TAG
+    echo "Remove eaxisting Nginx Image in Local"
+    docker rmi -f $LOCAL_NGINX_IMAGE_NAME:$LOCAL_NGINX_IMAGE_TAG
 fi
+echo "Create Nginx Image Tag in Local"
+docker build -t $LOCAL_NGINX_IMAGE_NAME:$LOCAL_NGINX_IMAGE_TAG -f Dockerfile_nginx .
+
+# Create NGINX Image Tag for NCR
+echo "Create NGINX Image Tag for NCR"
+docker tag $LOCAL_NGINX_IMAGE_NAME:$LOCAL_NGINX_IMAGE_TAG $NCR_ADDRESS/$NCR_NGINX_IMAGE_NAME:$NCR_NGINX_IMAGE_TAG
 
 # Push Image to NCR"
 echo "Push Django Image to Naver Container Registry"
