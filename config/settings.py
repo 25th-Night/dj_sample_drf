693522f64a0cfe25b225d863304cbf7dc0d1f377
25th-Night <brown_eyed87@naver.com>
2023-08-07 15:05:52 +0900

    config/settings: DB 정보를 환경변수에서 읽어오도록 수정

--- config/settings.py
+++ config/settings.py
@@ -103,10 +103,10 @@ WSGI_APPLICATION = "config.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "postgres",
-        "USER": "postgres",
-        "PASSWORD": "postgres",
-        "HOST": "postgres",
+        "NAME": os.getenv("POSTGRES_DB", 'postgres'),
+        "USER": os.getenv("POSTGRES_USER", 'postgres'),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD", 'postgres'),
+        "HOST": os.getenv("DB_HOST", 'postgres'),
     }
 }
 
