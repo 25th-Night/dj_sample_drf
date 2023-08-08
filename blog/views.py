4fa6a359addc84e9e064c487e54d8a22ada5d846
25th-Night <brown_eyed87@naver.com>
2023-08-08 13:39:41 +0900

    blog/views: create_blog 뷰 추가

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from pymongo import MongoClient
 
-# Create your views here.
+client = MongoClient()
+db = client.likelion  # db 생성
+
+
+def create_blog() -> bool:
+    blog = {
+        "title": "My first blog",
+        "content": "This is my first blog",
+        "author": "lion",
+    }
+    try:
+        db.blogs.insert_one(blog)  # blogs 라는 collection에 데이터 추가
+        return True
+    except Exception as e:
+        print(e)
+        return False
+
+def update_blog():
+    pass
+
+def delete():
+    pass
+
+def read_blog():
+    pass
