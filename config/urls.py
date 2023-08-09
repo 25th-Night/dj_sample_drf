3ef396c1599b333f42fbd470d7c1cf520a81a84f
25th-Night <brown_eyed87@naver.com>
2023-08-09 09:33:10 +0900

    config/urls: blog의 router를 url에 등록

--- config/urls.py
+++ config/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from blog.urls import router as blog_router
+
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("blog/", include("blog.urls")),
+    path("blog/", include(blog_router.urls)),
     path("api-auth/", include("rest_framework.urls")),
 ]
