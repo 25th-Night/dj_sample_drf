f38a2cb05fb509f7a5b74c806f702f7971cff105
25th-Night <brown_eyed87@naver.com>
2023-08-09 11:24:12 +0900

    forum/serializer: PostSerializer 추가

--- forum/serializers.py
+++ forum/serializers.py
@@ -12,3 +12,13 @@ class TopicSerializer(serializers.ModelSerializer):
             "updated_at",
         )
 
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = "__all__"
+        read_only_fields = (
+            "id",
+            "created_at",
+            "updated_at",
+        )
