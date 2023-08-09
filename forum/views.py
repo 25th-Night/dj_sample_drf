840e4260cca68422e1324aaec43215ce02bba47f
25th-Night <brown_eyed87@naver.com>
2023-08-09 11:23:58 +0900

    forum/views: PostViewSet 추가

--- forum/views.py
+++ forum/views.py
@@ -1,9 +1,13 @@
 from rest_framework import viewsets
 
-from .models import Topic
-from .serializers import TopicSerializer
+from .models import Post, Topic
+from .serializers import PostSerializer, TopicSerializer
 
 
 class TopicViewSet(viewsets.ModelViewSet):
     queryset = Topic.objects.all()
     serializer_class = TopicSerializer
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
