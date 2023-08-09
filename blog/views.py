18717fa385e8ba26fb0e97aee8ee76934864dba0
25th-Night <brown_eyed87@naver.com>
2023-08-09 09:32:26 +0900

    blog/views: BlogViewSet 생성 - DRF로 전환

--- blog/views.py
+++ blog/views.py
@@ -1,30 +1,36 @@
-from django.http import HttpResponse, JsonResponse
-from pymongo import MongoClient
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import status
 
-client = MongoClient(host="mongo")
-db = client.likelion  # db 생성
+from .serializers import BlogSerializer
 
 
-def create_blog(request) -> bool:
-    blog = {
-        "title": "My first blog",
-        "content": "This is my first blog",
-        "author": "lion",
-    }
-    try:
-        db.blogs.insert_one(blog)
-        # return JsonResponse({"status": True})
-        return HttpResponse("Success")
-    except Exception as e:
-        print(e)
-        # return JsonResponse({"status": False})
-        return HttpResponse("Failed")
+class BlogViewSet(ViewSet):
+    serializer_class = BlogSerializer
 
-def update_blog(request):
-    pass
+    def list(self, request):
+        return Response()
 
-def delete_blog(request):
-    pass
+    def create(self, request):
+        """
+        request.data = {
+            "title": "My first blog",
+            "content": "This is my first blog",
+            "author": "lion",
+        }
+        """
+        serializer = BlogSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.create(serializer.validated_data)
+            return Response(status=status.HTTP_201_CREATED, data=serializer.data)
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST, data=serializer.errors)
 
-def read_blog(request):
-    pass
+    def update(self, request):
+        pass
+
+    def retrieve(self, request):
+        pass
+
+    def destroy(self, request):
+        pass
