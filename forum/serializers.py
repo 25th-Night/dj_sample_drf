30a066091092efb633df36fb78047892a8b4b375
25th-Night <brown_eyed87@naver.com>
2023-08-11 15:37:31 +0900

    forum/serializer: serializer 순서 변경 및 코드 수정

--- forum/serializers.py
+++ forum/serializers.py
@@ -2,11 +2,26 @@ from rest_framework import serializers
 
 from .models import Topic, Post
 
+class PostSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Post
+        fields = "__all__"
+        read_only_fields = (
+            "id",
+            "created_at",
+            "updated_at",
+        )
+
+
+
+
 class TopicSerializer(serializers.ModelSerializer):
-    serializer = serializers.SerializerMethodField()
+    posts = serializers.SerializerMethodField()
     
     def get_posts(self, obj: Topic):
-        return obj.posts.all()
+        posts = obj.posts.all()
+        return PostSerializer(posts, many=True).data
 
     class Meta:
         model = Topic
@@ -19,18 +34,6 @@ class TopicSerializer(serializers.ModelSerializer):
             "updated_at",
             "posts",
         )
-        read_only_fields = (
-            "id",
-            "created_at",
-            "updated_at",
-        )
-
-
-class PostSerializer(serializers.ModelSerializer):
-    
-    class Meta:
-        model = Post
-        fields = "__all__"
         read_only_fields = (
             "id",
             "created_at",
