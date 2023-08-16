52c4cf7355f0ae53de963706ec3e9ac110cc30c6
25th-Night <brown_eyed87@naver.com>
2023-08-16 15:28:14 +0900

    forum/admin: TopicGroupUser를 admin site에 추가

--- forum/admin.py
+++ forum/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from forum.models import Post, Topic
+from forum.models import Post, Topic, TopicGroupUser
 
 
 admin.site.register(Topic)
 admin.site.register(Post)
+admin.site.register(TopicGroupUser)
