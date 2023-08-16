d5c056a86bbdbc3c249fb0aaf9c2674b71f19c8d
25th-Night <brown_eyed87@naver.com>
2023-08-16 21:45:35 +0900

    lb domain을 환경변수로 분리

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -8,10 +8,9 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 DEBUG = False
 
 ALLOWED_HOSTS = [
-    # production 환경의 Django 서버의 LB 도메인
-    "lion-lb-18904314-4889fba25a98.kr.lb.naverncp.com",
+    os.getenv("NCP_LB_DOMAIN"),
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    "http://lion-lb-18904314-4889fba25a98.kr.lb.naverncp.com",
+    f"http://{os.getenv('NCP_LB_DOMAIN')}",
 ]
--- config/settings/staging.py
+++ config/settings/staging.py
@@ -5,14 +5,12 @@ from .base import *
 
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
-
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    # staging 환경의 Django 서버의 LB 도메인
-    "lion-staging-lb-18974486-d2dd79860392.kr.lb.naverncp.com",
+    os.getenv("NCP_LB_DOMAIN"),
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    "http://lion-staging-lb-18974486-d2dd79860392.kr.lb.naverncp.com",
+    f"http://{os.getenv('NCP_LB_DOMAIN')}",
 ]
