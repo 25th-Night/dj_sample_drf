9260486ac369cd37420c44df668ea5caf018394e
25th-Night <brown_eyed87@naver.com>
2023-08-17 10:38:59 +0900

    forum/admin: TopicGroupUser 수정

--- forum/admin.py
+++ forum/admin.py
@@ -5,4 +5,12 @@ from forum.models import Post, Topic, TopicGroupUser
 
 admin.site.register(Topic)
 admin.site.register(Post)
-admin.site.register(TopicGroupUser)
+
+
+@admin.register(TopicGroupUser)
+class TopicGroupUSerAdmin(admin.ModelAdmin):
+    list_display = ["topic", "group", "user"]
+
+    @admin.display(description="group")
+    def get_group_at(self, obj):
+        return obj.get_group_display()
