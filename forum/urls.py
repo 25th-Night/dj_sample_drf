65bae93066f5a59bef9249d16ebfce78ad9ea128
25th-Night <brown_eyed87@naver.com>
2023-08-17 19:46:42 +0900

    forum.urls: 불필요한 코드 제거

--- forum/urls.py
+++ forum/urls.py
@@ -4,14 +4,5 @@ from .views import TopicViewSet, PostViewSet
 
 router = DefaultRouter()
 
-
-# router.register(r'topics', TopicViewSet)
-# router.register(
-#     r'topics/(?P<topic_id>\d+)/posts',
-#     PostViewSet,
-#     basename='topic-posts'
-# )
-
 router.register("topics", TopicViewSet, basename="topic")
-# router.register("topics/<int:topic_id>/posts", PostViewSet, basename="topic-posts")
 router.register("posts", PostViewSet, basename="post")
