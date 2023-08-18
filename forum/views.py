81eb92215e60ea067d3fa179248f166aff06d7f7
25th-Night <brown_eyed87@naver.com>
2023-08-18 11:01:40 +0900

    forum/views: override retrieve method

--- forum/views.py
+++ forum/views.py
@@ -81,3 +81,16 @@ class PostViewSet(viewsets.ModelViewSet):
             )
         else:
             return Response(status=status.HTTP_400_BAD_REQUEST, data=serializer.errors)
+
+    def retrieve(self, request: Request, *args, **kwargs):
+        post: Post = self.get_object()
+        user = request.user
+        topic = post.topic
+
+        if not topic.can_be_access_by(user):
+            return Response(
+                status=status.HTTP_401_UNAUTHORIZED,
+                data="This user is not allowed to write a post on this topic",
+            )
+
+        return super().retrieve(request, *args, **kwargs)
