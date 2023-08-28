28f0b271c355628c36f4317338c5ca4f21d321e6
25th-Night <brown_eyed87@naver.com>
2023-08-28 10:07:15 +0900

    infra/staging/main: backend 추가(local, pg)

--- infra/staging/main.tf
+++ infra/staging/main.tf
@@ -1,4 +1,10 @@
 terraform {
+  backend "local" {
+    path = "/Users/brown/Study/Lecture/TI/terraform_study/states/staging.tfstate"
+  }
+
+  #   backend "pg" {}
+
   required_providers {
     ncloud = {
       source = "NaverCloudPlatform/ncloud"
