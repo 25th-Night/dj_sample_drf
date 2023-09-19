4e400bd561fef8b92f33750c6570efa9b0833bd0
25th-Night <brown_eyed87@naver.com>
2023-09-19 09:30:50 +0900

    common/middleware: 미들웨어에서 버전으로 헬스체크

--- common/middleware.py
+++ common/middleware.py
@@ -1,4 +1,5 @@
 from django.http import JsonResponse
+from django.conf import settings
 
 
 class HealthCheckMiddleware:
@@ -6,7 +7,20 @@ class HealthCheckMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
+        if settings.VERSION == "0.3.0":
+            return JsonResponse({"status": "unhealthy"}, status=500)
         if request.path == "/health/":
             return JsonResponse({"status": "ok"})
 
         return self.get_response(request)  # 그다음 미들웨어로 보냄
+
+
+# class VersionCheckMiddleware:
+#     def __init__(self, get_response):
+#         self.get_response = get_response
+
+#     def __call__(self, request):
+#         if request.path == "/version/":
+#             return JsonResponse({"version": settings.VERSION})
+
+#         return self.get_response(request)  # 그다음 미들웨어로 보냄
