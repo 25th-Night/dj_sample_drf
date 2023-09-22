6e4335791736d92ee42469c5ba8a20f3edddba4d
25th-Night <brown_eyed87@naver.com>
2023-09-22 13:32:35 +0900

    config/settings/base: ver 0.3.2 수정

--- config/settings/base.py
+++ config/settings/base.py
@@ -19,7 +19,7 @@ from common.aws import get_secret
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
-VERSION = "0.3.1"
+VERSION = "0.3.2"
 
 
 # Quick-start development settings - unsuitable for production
